79f8412f5f0f303aad5b3072f97234d4f20318d2
Enzo_Gatica <enzogatica96@gmail.com>
2023-06-28 20:46:18 -0400

    termino de diseño, agregar el Toast

--- app/src/main/java/com/example/disenoboton/MainActivity.java
+++ app/src/main/java/com/example/disenoboton/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.disenoboton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.disenoboton.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        binding.btn1.setOnClickListener(view -> Toast.makeText(this,"Boton 1 selecionado", Toast.LENGTH_SHORT).show());
+        binding.btn2.setOnClickListener(view -> Toast.makeText(this,"Boton 2 selecionado", Toast.LENGTH_SHORT).show());
+        binding.btn3.setOnClickListener(view -> Toast.makeText(this,"Boton 3 selecionado", Toast.LENGTH_SHORT).show());
+        binding.btn4.setOnClickListener(view -> Toast.makeText(this,"Boton 4 selecionado", Toast.LENGTH_SHORT).show());
+        binding.btn5.setOnClickListener(view -> Toast.makeText(this,"Boton 5 selecionado", Toast.LENGTH_SHORT).show());
+        binding.btn6.setOnClickListener(view -> Toast.makeText(this,"Boton 6 selecionado", Toast.LENGTH_SHORT).show());
+
+        /*
+        binding.btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getBaseContext(), "Boton1 selecionado", Toast.LENGTH_SHORT).show();
+            }
+        });
+        */
+
     }
 }
